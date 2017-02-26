5c12c2ddf5dc1796fb2053db8428f635ad43e575
Wes Hediger <wes.hediger@gmail.com>
2017-02-26 00:07:31 -0700

    add migration files

--- robinhood/knexfile.js
+++ robinhood/knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/robin_hood'
+    connection: 'postgres://localhost/local-strategy'
   },
 
   production: {
