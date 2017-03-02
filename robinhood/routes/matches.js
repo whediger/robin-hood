09b772e962f3d73518e999df8ee85ce4d790ffda
Wes Hediger <wes.hediger@gmail.com>
2017-03-01 20:30:11 -0700

    add date formatter

--- robinhood/routes/matches.js
+++ robinhood/routes/matches.js
@@ -1,12 +1,13 @@
 var express = require('express');
-var db = require('../js/database')
-var appjs = require('../js/appjs')
+var db = require('../js/database');
+var appjs = require('../js/appjs');
 var router = express.Router();
 
 //NOTE No route to 'matches' used
 router.get('/', function(req, res, next) {
   db.getMatches()
   .then(function(data){
+    appjs.matchDateFormatter(data);
     res.render('matches', { matches: data });
   });
 });
