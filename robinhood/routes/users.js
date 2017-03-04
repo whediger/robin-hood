3ebb0f9d073962ca1c5b6b17da229d72d851f65b
Wes Hediger <wes.hediger@gmail.com>
2017-03-04 14:18:06 -0700

    add new user route

--- robinhood/routes/users.js
+++ robinhood/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/newuser', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('newuser');
 });
 
 module.exports = router;
